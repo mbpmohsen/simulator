4f91f99e5f6c952cb7809bc3b75f80d95cf08e17
mohsen.babaei <mbp.moh3n@gmail.com>
2026-02-19 14:34:11 +0330

    remove login section

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,18 +1,5 @@
-import Image from "next/image";
-import AdminLoginBackground from "@/assets/admin-login-bg.svg";
-import LoginCard from "@/components/LoginCard";
+import { redirect } from "next/navigation";
 
 export default function LoginPage() {
-	return (
-		<div className="fixed inset-0 -z-10">
-			<Image
-				alt="admin login background"
-				src={AdminLoginBackground.src}
-				fill
-				className="object-cover"
-				sizes="100svw"
-			/>
-			<LoginCard />
-		</div>
-	);
+	redirect("/configuration");
 }
